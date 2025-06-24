1b24c4ea0d21775a1bc92de8078b7f09e1f1efac
CorvoQueso <corvoqueso@gmail.com>
2025-06-24 23:27:57 +0100

    u wot m8

---
+++ file writing program.py
@@ -0,0 +1,24 @@
+import pathlib
+import os
+#rember: counting starts at 0
+#trucate means to remove content from the end of a file 
+
+#print(os.path.expanduser("~"))
+
+os.system('mkdir ~/Desktop/thimble')
+
+#with open(os.path.expanduser("~") + "/Documents/VScode/Local/idk/readTest.txt") as fp:
+#    deez=fp.readlines() #[1]
+#    print(deez)
+#    for i, line in enumerate(deez):
+#        if 'string' in line:
+#            deez.insert(i,"i met bro in a coffee shop")
+#    fp.seek(3)
+#    fp.writelines(deez)
+
+
+
+#with open(mode='a', file=os.path.expanduser("~") + "/Documents/VScode/local/idk/readTest.txt") as ch:
+#    ch.seek(3)
+#    onions=ch.writelines("wadahel")
+#    ch.close
